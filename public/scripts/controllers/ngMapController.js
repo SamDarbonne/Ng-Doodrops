5de7cbde52f20dc8786d8f4a119af8c49e9ab70f
samdarbonne <samdarbonne@gmail.com>
2016-09-22 15:12:53 -0700

    adds editing to front end

--- public/scripts/controllers/ngMapController.js
+++ public/scripts/controllers/ngMapController.js
@@ -1,6 +1,12 @@
-angular.module('ngMap').run(function($rootScope, NgMap) {
-  NgMap.getMap().then(function(map) {
-    $rootScope.map = map;
-
-  });
-});
+//angular.module('ngdrops', ['ngMap'])//.controller('autocompleteController', function(NgMap) {
+  // var vm = this;
+  // vm.types = "['establishment']";
+  // vm.placeChanged = function() {
+  //   vm.place = this.getPlace();
+  //   console.log('location', vm.place.geometry.location);
+  //   vm.map.setCenter(vm.place.geometry.location);
+  // }
+  // NgMap.getMap().then(function(map) {
+  //   vm.map = map;
+  // });
+ //});
